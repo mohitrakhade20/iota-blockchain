8b6f2aa198056ab771006877732089cb906d27c3
Mohit Madhukar Rakhade <48031017+mohitrakhade20@users.noreply.github.com>
2020-10-05 18:04:57 +0530

    seed generated

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const iota = composeAPI({
   provider: "https://nodes.devnet.iota.org:443",
 });
 
-console.log("Connected to IOTA devnet :) ");
+console.log("Connected to IOTA devnet :) :)");
 
 iota
   .getNodeInfo()
@@ -14,3 +14,24 @@ iota
   .catch((error) => {
     console.log(`Request Error : ${error.message}`);
   });
+
+let seed =
+  "SBZOFNTMVIQKXRAC9GC9RVBOXWMWQAVTOYFBBZMKNDRQPNJXRYBWVPLEPUYZGGVXULDIELQVWR9KFILYE";
+
+let addresses = iota.getNewAddress(
+  seed,
+  {
+    index: 0,
+    total: 1,
+    security: 2,
+    checksum: true,
+    returnAll: false,
+  },
+  (error, success) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(success[0]);
+    }
+  }
+);
